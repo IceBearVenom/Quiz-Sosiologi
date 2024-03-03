9ad2756d02a14026f11646200666f6b31756c2db
IceBearVenom <tristansbastian848@gmail.com>
2024-03-03 10:16:13 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,151 @@
+const questions = [
+    {
+        question: 'SAM Kominfo Bidang Hukum menjelaskan pertanyaan yang mengemuka kenapa ada pihak yang melakukan peretasan dan bagaimana caranya. “Pertama, bahwa  secara disiplin mengatakan bahwa hacking itu terjadi karena SQL atau adanya  injection SQL dimana hacker itu mengakses data lewat server melalui kesalahan yang ada dalam filter inputnya. Jadi, lewat servernya dia menyerang, mencoba masuk ke servernya,” ungkapnya. Selanjutnya, hal kedua menurut SAM Hendri bahwa ada istilah IDOR (Insecure Direct Object References) artinya hacker mengakses data  akun pengguna milik orang lain kemudian melalui akun itu dia masuk ke akun-akun lain. “Ini sering terjadi yaitu akun kita dihack kemudian akun teman-teman kita dipake, dimasuki oleh mereka,” ungkapnya. Hal lainnya menurut SAM Kominfo Bidang Hukum kenapa muncul peretasan bahwa ada juga  istilah CVE (Common Vulnerabilities and Exposures) yaitu aplikasi yg dipakai sudah tidak terupdate lagi. “Kita sering kali punya aplikasi, itu tidak kita update macem-macem. Ini kadang-kadang karena sudah lupa passwordnya atau memang hardwarenya bukan yang asli sehingga tidak ada updating. Itu seringkali terjadi,” paparnya. Keempat yang selanjutnya diungkap SAM Henri bahwa terdapat human error. “Kadang-kadang ada kesengajaan terutama kalau eror itu karena minimnya edukasi tentang kerahasiaan data atau juga hal-hal lain terkait dengan lemahnya keamanan cyber (cyber security),” ungkapnya.<br>Sumber: https://www.kominfo.go.id/content/detail/28843/lindungi-data-pribadi-inilah-modus-ancaman-peretasan/0/berita_satker<br><br>Berdasarkan teks diatas, apa gejala sosial yang sesuai?',
+        answers: [
+            { text: 'Kemiskinan', correct: false},
+            { text: 'Ekonomi', correct: false},
+            { text: 'Modernisasi', correct: false},
+            { text: 'Teknologi', correct: true},
+        ]
+    },
+    {
+        question: 'SAM Kominfo Bidang Hukum menjelaskan pertanyaan yang mengemuka kenapa ada pihak yang melakukan peretasan dan bagaimana caranya. “Pertama, bahwa  secara disiplin mengatakan bahwa hacking itu terjadi karena SQL atau adanya  injection SQL dimana hacker itu mengakses data lewat server melalui kesalahan yang ada dalam filter inputnya. Jadi, lewat servernya dia menyerang, mencoba masuk ke servernya,” ungkapnya. Selanjutnya, hal kedua menurut SAM Hendri bahwa ada istilah IDOR (Insecure Direct Object References) artinya hacker mengakses data  akun pengguna milik orang lain kemudian melalui akun itu dia masuk ke akun-akun lain. “Ini sering terjadi yaitu akun kita dihack kemudian akun teman-teman kita dipake, dimasuki oleh mereka,” ungkapnya. Hal lainnya menurut SAM Kominfo Bidang Hukum kenapa muncul peretasan bahwa ada juga  istilah CVE (Common Vulnerabilities and Exposures) yaitu aplikasi yg dipakai sudah tidak terupdate lagi. “Kita sering kali punya aplikasi, itu tidak kita update macem-macem. Ini kadang-kadang karena sudah lupa passwordnya atau memang hardwarenya bukan yang asli sehingga tidak ada updating. Itu seringkali terjadi,” paparnya. Keempat yang selanjutnya diungkap SAM Henri bahwa terdapat human error. “Kadang-kadang ada kesengajaan terutama kalau eror itu karena minimnya edukasi tentang kerahasiaan data atau juga hal-hal lain terkait dengan lemahnya keamanan cyber (cyber security),” ungkapnya.<br>Sumber: https://www.kominfo.go.id/content/detail/28843/lindungi-data-pribadi-inilah-modus-ancaman-peretasan/0/berita_satker<br><br>Apa yang menyebabkan pencurian data tersebut?',
+        answers: [
+            { text: 'Penggelembungan Teknologi', correct: false},
+            { text: 'Perkembangan Teknologi', correct: true},
+            { text: 'Penyusutan Teknologi', correct: false},
+            { text: 'Pengempisan Teknologi', correct: false},
+        ]
+    },
+    {
+        question: 'SAM Kominfo Bidang Hukum menjelaskan pertanyaan yang mengemuka kenapa ada pihak yang melakukan peretasan dan bagaimana caranya. “Pertama, bahwa  secara disiplin mengatakan bahwa hacking itu terjadi karena SQL atau adanya  injection SQL dimana hacker itu mengakses data lewat server melalui kesalahan yang ada dalam filter inputnya. Jadi, lewat servernya dia menyerang, mencoba masuk ke servernya,” ungkapnya. Selanjutnya, hal kedua menurut SAM Hendri bahwa ada istilah IDOR (Insecure Direct Object References) artinya hacker mengakses data  akun pengguna milik orang lain kemudian melalui akun itu dia masuk ke akun-akun lain. “Ini sering terjadi yaitu akun kita dihack kemudian akun teman-teman kita dipake, dimasuki oleh mereka,” ungkapnya. Hal lainnya menurut SAM Kominfo Bidang Hukum kenapa muncul peretasan bahwa ada juga  istilah CVE (Common Vulnerabilities and Exposures) yaitu aplikasi yg dipakai sudah tidak terupdate lagi. “Kita sering kali punya aplikasi, itu tidak kita update macem-macem. Ini kadang-kadang karena sudah lupa passwordnya atau memang hardwarenya bukan yang asli sehingga tidak ada updating. Itu seringkali terjadi,” paparnya. Keempat yang selanjutnya diungkap SAM Henri bahwa terdapat human error. “Kadang-kadang ada kesengajaan terutama kalau eror itu karena minimnya edukasi tentang kerahasiaan data atau juga hal-hal lain terkait dengan lemahnya keamanan cyber (cyber security),” ungkapnya.<br>Sumber: https://www.kominfo.go.id/content/detail/28843/lindungi-data-pribadi-inilah-modus-ancaman-peretasan/0/berita_satker<br><br>Apakah dampak yang akan terjadi di masyarakat?',
+        answers: [
+            { text: 'Pemerintah meningkatkan keamanan', correct: false},
+            { text: 'Masyarakat mejadi panik', correct: false},
+            { text: 'Hacker merajalela', correct: false},
+            { text: 'Reputasi pemerintah menjadi buruk', correct: true},
+        ]
+    }
+];
+
+const questionElement = document.getElementById('question');
+const answerButton = document.getElementById('answer');
+const nextButton = document.getElementById('next');
+
+let currentquestionindex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentquestionindex = 0;
+    score = 0;
+    nextButton.innerHTML = 'Next';
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentquestionindex];
+    let questionNo = currentquestionindex + 1;
+    questionElement.innerHTML = questionNo + '. ' + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer =>{
+        const button = document.createElement('button');
+        button.innerHTML = answer.text;
+        button.classList.add('btn');
+        answerButton.appendChild(button)
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer);
+    });
+}
+
+function resetState(){
+    nextButton.style.display = 'none';
+    while(answerButton.firstChild){
+        answerButton.removeChild(answerButton.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === 'true';
+    if(isCorrect){
+        selectedBtn.classList.add('correct');
+        score++;
+    }else{
+        selectedBtn.classList.add('incorrect')
+    }
+    Array.from(answerButton.children).forEach(button => {
+        if(button.dataset.correct === 'true'){
+            button.classList.add('correct');
+        }
+        button.disabled = true;
+    })
+    nextButton.style.display = 'block'
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = ' You scored ' + score + ' out of ' + questions.length + '!';
+    nextButton.innerHTML = 'Play Again';
+    nextButton.style.display = 'block'
+}
+
+function handleNextButton(){
+    currentquestionindex++;
+    if(currentquestionindex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener('click', ()=>{
+    if(currentquestionindex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+})
+
+startQuiz();
+
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+var W = window.innerWidth;
+let H = 1500;
+
+if(W > 768){
+    H = 1100;
+}
+
+canvas.width = W;
+canvas.height = H;
+
+var fontSize = 16;
+var columns = Math.floor(W / fontSize);
+var drops = [];
+for(var i=0; i<columns; i++){
+    drops.push(0);
+}
+var str = "JavaScript Hacking Effect";
+function draw(){
+    context.fillStyle = "rgba(0,0,0,0.05)";
+    context.fillRect(0, 0, W, H);
+    context.fontSize = "700 " + fontSize + "px";
+    context.fillStyle = "#00cc33";
+    for(var i=0; i<columns; i++){
+        var index = Math.floor(Math.random()*str.length);
+        var x = i * fontSize;
+        var y = drops[i] * fontSize;
+        context.fillText(str[index], x, y);
+        if(y >= canvas.height && Math.random() > 0.99){
+            drops[i] = 0;
+        }
+        drops[i]++;
+    }
+}
+
+draw();
+setInterval(draw, 35);
